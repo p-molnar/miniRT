e592187e7b14407c0d1511d457152136b708a834
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-13 13:20:52 +0200

    Add function prototypes

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,13 +6,19 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:01:05 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 12:01:30 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/13 13:12:33 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
+# include <mrt_data_struct.h>
 
+//	parser
+void	parse_scene(t_scn *scn, int argc, char *argv[]);
+
+//	error handling
+int		error(char *msg, int exit_method, int exit_code);
 
 #endif
