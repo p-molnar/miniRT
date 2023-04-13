4fd6f67132ffd1cc07457199017deccfeaec30bf
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-13 13:22:07 +0200

    Add file

---
+++ inc/mrt_macros.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   mrt_macros.h                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/13 12:52:36 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/13 12:59:27 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MRT_MACROS_H
+# define MRT_MACROS_H
+
+enum	e_error_handler
+{
+	EXIT = 1,
+	RETURN = 2,
+};
+
+#endif
