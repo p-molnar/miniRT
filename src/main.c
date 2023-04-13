0ee8746b04ca9be682e503723bc08c8925aee6e6
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-13 13:24:59 +0200

    Add scene parser

--- src/main.c
+++ src/main.c
@@ -6,8 +6,18 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:00:14 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 12:00:27 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/13 13:20:11 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
+#include <mrt_data_struct.h>
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	t_scn	scn;
+
+	parse_scene(&scn, argc, argv);
+	return (EXIT_SUCCESS);
+}
