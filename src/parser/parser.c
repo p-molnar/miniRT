1e0ef7e24ac45bd2e8631fea68b95971a67b80a9
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-13 13:24:55 +0200

    Add file

---
+++ src/parser/parser.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parser.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/13 11:55:08 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/13 13:13:03 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <minirt.h>
+#include <mrt_macros.h>
+
+void	parse_scene(t_scn *scn, int argc, char *argv[])
+{
+	if (argc != 2)
+		error(ft_strdup("Invalid argument count"), EXIT, 1);
+}
