100ff8f7c7e5559243d1f018f1d9f4b63b667aba
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-16 20:32:07 +0200

    Add scene initialiser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:00:14 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 13:20:11 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/14 14:13:42 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 #include <mrt_data_struct.h>
 #include <stdlib.h>
 
+void	init_scene(t_scn *scn)
+{
+	scn->els = NULL;
+}
+
 int	main(int argc, char *argv[])
 {
 	t_scn	scn;
 
+	init_scene(&scn);
 	parse_scene(&scn, argc, argv);
 	return (EXIT_SUCCESS);
 }
