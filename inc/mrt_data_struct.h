e2063299a8059d5f8c11e8f1f7677d360a58fd3e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-16 20:28:59 +0200

    Collapse separarte element structs to one
    Each scene element had its own struct. This commit creates a generic
    scene element struct, that bears with all the members that an element
    shell have.

--- inc/mrt_data_struct.h
+++ inc/mrt_data_struct.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:06:43 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 13:08:07 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/16 20:02:27 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,54 +29,66 @@ typedef struct s_coord_3d
 	int	z;	
 }	t_coord_3d;
 
-typedef struct s_cam
+// typedef struct s_cam
+// {
+// 	int				type;
+// 	t_coord_3d		pos;
+// 	t_coord_3d		vec;
+// 	unsigned int	fov;
+// }	t_cam;
+
+// typedef struct s_amb_light
+// {
+// 	int				type;
+// 	unsigned int	brightness;
+// 	t_color			color;
+// }	t_amb_light;
+
+// typedef struct s_sphere
+// {
+// 	int				type;
+// 	t_coord_3d		pos;
+// 	int				diamtr;
+// 	t_color			color;
+// }	t_sphere;
+
+// typedef struct s_plane
+// {
+// 	int				type;
+// 	t_coord_3d		pos;
+// 	t_coord_3d		vec;
+// 	t_color			color;
+// }	t_plane;
+
+// typedef struct s_cylinder
+// {
+// 	int				type;
+// 	t_coord_3d		pos;
+// 	t_coord_3d		vec;
+// 	int				diamtr;
+// 	int				height;
+// 	t_color			color;
+// }	t_cylinder;
+
+// typedef struct s_light
+// {	
+// 	int				type;
+// 	t_coord_3d		pos;
+// 	unsigned int	brightness;
+// 	t_color			color;
+// }	t_light;
+
+typedef struct s_scn_el
 {
-	char			*type;
+	int				type;
 	t_coord_3d		pos;
 	t_coord_3d		vec;
-	unsigned int	fov;
-}	t_cam;
-
-typedef struct s_amb_light
-{
-	char			*type;
-	unsigned int	brightness;
-	t_color			color;
-}	t_amb_light;
-
-typedef struct s_sphere
-{
-	char			*type;
-	t_coord_3d		pos;
-	int				diamtr;
-	t_color			color;
-}	t_sphere;
-
-typedef struct s_plane
-{
-	char			*type;
-	t_coord_3d		pos;
-	t_coord_3d		vec;
-	t_color			color;
-}	t_plane;
-
-typedef struct s_cylinder
-{
-	char			*type;
-	t_coord_3d		pos;
-	t_coord_3d		vec;
-	int				diamtr;
+	int				diameter;
 	int				height;
-	t_color			color;
-}	t_cylinder;
-
-typedef struct s_light
-{
-	char			*type;
-	t_coord_3d		pos;
+	unsigned int	fov;
 	unsigned int	brightness;
 	t_color			color;
-}	t_light;
+}	t_scn_el;
 
 typedef struct s_scn
 {
