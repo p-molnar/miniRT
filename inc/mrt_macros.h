3fa8467d546d65234452b1e66376ae7acb6f40b5
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-16 20:30:17 +0200

    Add scn element type enum

--- inc/mrt_macros.h
+++ inc/mrt_macros.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:52:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 12:59:27 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/16 20:26:49 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ enum	e_error_handler
 {
 	EXIT = 1,
 	RETURN = 2,
+};
+
+enum	e_scn_el_type
+{
+	AMB_LIGHT=1,
+	LIGHT,
+	CAM,
+	SPHERE,
+	PLANE,
+	CYLYNDER
 };
 
 #endif
