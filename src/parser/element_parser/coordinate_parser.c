2cb5a7749c5d3c33b34a09bcf48645191e5cd8ed
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-17 14:12:25 +0200

    Add file

---
+++ src/parser/element_parser/coordinate_parser.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   coordinate_parser.c                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/17 09:35:12 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/17 14:06:58 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minirt.h>
+#include <mrt_macros.h>
+
+void	parse_coordinates(float *coord, char *input)
+{
+	char	**coords;
+	int		i;
+	float	f[3];
+
+	coords = ft_split(input, ',');
+	if (!coords)
+		error(ft_strdup("Malloc error: __FILE__: __LINE__"), EXIT, 1);
+	if (get_arr_size(coords) != 3)
+		error(ft_strdup("Required coordinates: x, y, x"), EXIT, 1);
+	i = 0;
+	while (coords[i])
+	{
+		if (!is_valid_number(coords[i]))
+			error(strconcat(2, "Invalid number: ", coords[i]), EXIT, 1);
+		f[i] = ft_atof(coords[i]);
+		coord[i] = f[i];
+		i++;
+	}
+	free_arr(coords);
+}
+
+void	parse_norm_vec(float *coord, char *input)
+{
+	char	**coords;
+	int		i;
+	float	f[3];
+
+	coords = ft_split(input, ',');
+	if (!coords)
+		error(ft_strdup("Malloc error: __FILE__: __LINE__"), EXIT, 1);
+	if (get_arr_size(coords) != 3)
+		error(ft_strdup("Required coordinates: x, y, x"), EXIT, 1);
+	i = 0;
+	while (coords[i])
+	{
+		if (!is_valid_number(coords[i]))
+			error(strconcat(2, "Invalid number: ", coords[i]), EXIT, 1);
+		f[i] = ft_atof(coords[i]);
+		if (!is_in_range_f(f[i], -1.0, 1.0))
+			error(strconcat(2, "Number out of range [-1.0, 1.0]: ", coords[i]),
+				EXIT, 1);
+		coord[i] = f[i];
+		i++;
+	}
+	free_arr(coords);
+}
