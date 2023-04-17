cfcd091f7e710dfb86e59b3b2517a102d00c5ed2
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-17 14:07:54 +0200

    Add function prototypes

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:01:05 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/13 13:12:33 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/17 13:55:53 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@
 
 //	parser
 void	parse_scene(t_scn *scn, int argc, char *argv[]);
+void	parse_elements(t_scn_el *el, char **input, unsigned int flags);
+void	parse_type_identifier(t_scn_el *el, char *line);
+
+//	element parser
+void	parse_coordinates(float *coord, char *input);
+void	parse_norm_vec(float *coord, char *input);
+void	parse_float(float *f, char *input);
+void	parse_range(float *f, char *input, float n_lower, float n_upper);
+void	parse_color(unsigned int *color, char *input, int n_lower, int n_upper);
+
+//	free
+void	free_arr(char **arr);
+
+//	parser utils
+int		get_arr_size(char **arr);
+int		is_in_range_f(float n, float n_lower, float n_upper);
+int		is_in_range_i(int n, int n_lower, int n_upper);
+int		is_valid_number(char *c);
 
 //	error handling
 int		error(char *msg, int exit_method, int exit_code);
