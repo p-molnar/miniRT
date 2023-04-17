f8f37d1211c4657ef05827eb3a952fb2a6f29810
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-17 14:10:08 +0200

    Fix array free
    The function segfaulted, because of double free. The issue is solved by
    accessing the array elements via random access instead of pointer
    arithmetics.

--- src/free/free.c
+++ src/free/free.c
@@ -6,14 +6,19 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/16 19:24:09 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/16 19:25:24 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/17 13:01:24 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 void	free_arr(char **arr)
 {
-	while (arr && *arr)
-		free(arr++);
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+		free(arr[i++]);
 	if (arr)
 		free(arr);
 }
