abe112a6aabf21420579e3ae85fa8a1d417b2b1b
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-18 14:33:56 +0200

    Add scene generator py script

---
+++ scenes/scene_generator.py
@@ -0,0 +1,16 @@
+def gen_cam_config:
+
+def gen_light_config:
+
+def gen_amb_light_config:
+
+def	gen_pl
+def	gen_sp
+def	gen_cy
+
+
+def main:
+	gen_amb_light_config()
+	def_cam_config()
+	gen_light_config()
+
