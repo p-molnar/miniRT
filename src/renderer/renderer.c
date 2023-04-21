1a26064a44ba2d3e08a44a71568438b5e9812a49
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-21 12:14:04 +0200

    Add file

---
+++ src/renderer/renderer.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   renderer.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/21 11:13:10 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/21 11:16:02 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minirt.h>
+#include <libft.h>
+#include <MLX42.h>
+
+void	render_elements(t_data *scn)
+{
+	while ()
+}
