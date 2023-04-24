f9103407a8c3399c6c5666aa3a196bc1b4cf5af2
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-25 00:00:50 +0200

    Rearrange function call

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:00:14 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/21 21:16:29 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/24 23:51:43 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,11 @@ void	print_scene_el(t_data *scn)
 		printf("height: %f\n", ((t_scn_el *)el->content)->height);
 		printf("fov: %f\n", ((t_scn_el *)el->content)->fov);
 		printf("brightness: %f\n", ((t_scn_el *)el->content)->brightness);
-		printf("color: x=%d, y=%d, z=%d\n", ((t_scn_el *)el->content)->color[0],
-			((t_scn_el *)el->content)->color[1],
-			((t_scn_el *)el->content)->color[2]);
+		// printf("color: r=%d, g=%d, b=%d, a=%d\n",
+		// 	get_r(((t_scn_el *)el->content)->color), 
+		// 	get_g(((t_scn_el *)el->content)->color), 
+		// 	get_b(((t_scn_el *)el->content)->color), 
+		// 	get_a(((t_scn_el *)el->content)->color));
 		el = el->next;
 	}
 }
@@ -58,17 +60,17 @@ int	main(int argc, char *argv[])
 
 	init_scene(&d);
 	parse_scene(&d, argc, argv);
-	d.mlx = mlx_init(WIDTH, HEIGHT, "MiniRT", true);
+	// print_scene_el(&d);
+	d.mlx = mlx_init(WIDTH+5, HEIGHT+5, "MiniRT", true);
 	if (!d.mlx)
 		error(ft_strdup(mlx_strerror(mlx_errno)), EXIT, 1);
-	d.img = mlx_new_image(d.mlx, 256, 256);
+	d.img = mlx_new_image(d.mlx, WIDTH, HEIGHT);
 	if (!d.img || (mlx_image_to_window(d.mlx, d.img, 0, 0) < 0))
 		error(ft_strdup(mlx_strerror(mlx_errno)), EXIT, 1);
 	render_img(&d);
 	// mlx_loop_hook(d.mlx, ft_hook, d.mlx);
 	mlx_loop(d.mlx);
 	mlx_terminate(d.mlx);
-	// print_scene_el(&scn);	
 	// system("leaks minirt");
 	return (EXIT_SUCCESS);
 }
