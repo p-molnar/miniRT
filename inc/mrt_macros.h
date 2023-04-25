47a0429cc1086e33fdab23b4a3e95731056dccb0
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-25 14:52:05 +0200

    Remove viewport

--- inc/mrt_macros.h
+++ inc/mrt_macros.h
@@ -6,17 +6,15 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:52:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/24 23:26:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/25 14:48:40 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MRT_MACROS_H
 # define MRT_MACROS_H
-# define WIDTH 500
-# define HEIGHT 500
-# define VIEWPORT_W 500
-# define VIEWPORT_H 500
-# define INF 99999999
+# define WIDTH 800
+# define HEIGHT 800
+# define INF 9999 
 
 enum	e_error_handler
 {
@@ -48,7 +46,7 @@ enum	e_scn_el_type
 	CAM,
 	SPHERE,
 	PLANE,
-	CYLYNDER
+	CYLYNDER,
 };
 
 enum	e_scn_el_member_flags
