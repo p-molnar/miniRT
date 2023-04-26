4579fb58debc1e9787f92878a219abc3b9c8a29a
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-26 11:34:03 +0200

    Refactor code

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 11:13:10 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/26 10:09:32 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/26 11:08:57 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 #include <mrt_macros.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define CO 0
+#define D 1
 
 long double	*canvas_to_viewport(int x, int y, long double *viewport)
 {
@@ -32,47 +35,43 @@ long double	*canvas_to_viewport(int x, int y, long double *viewport)
 }
 
 long double	*get_intersection_points(long double *cam_position,
-		long double *proj_plane, t_scn_el *obj)
+										long double *proj_plane,
+										t_scn_el *obj)
 {
 	long double	r;
-	t_vec		*CO;
-	t_vec		*D;
-	long double	a;
-	long double	b;
-	long double	c;
+	t_vec		*vec[2];
+	long double	param[3];
 	long double	discriminant;
-	long double	*arr;
+	long double	*t;
 
 	r = obj->diameter / 2;
 	// CO = get_dir_vec(cam_position, obj->coord);
-	CO = get_dir_vec(obj->coord, cam_position);
-	D = get_dir_vec(cam_position, proj_plane);
-	a = dot(D, D);
-	b = 2.0 * dot(CO, D);
-	c = dot(CO, CO) - r * r;
-	discriminant = b * b - 4 * a * c;
-	arr = malloc(2 * sizeof(long double));
-	arr[0] = INF;
-	arr[1] = INF;
-	if (discriminant >= 0)
+	vec[CO] = get_dir_vec(obj->coord, cam_position);
+	vec[D] = get_dir_vec(cam_position, proj_plane);
+	param[0] = dot(vec[D], vec[D]);
+	param[1] = 2.0 * dot(vec[CO], vec[D]);
+	param[2] = dot(vec[CO], vec[CO]) - r * r;
+	t = quad_eq_solver(param[0], param[1], param[2], &discriminant);
+	if (discriminant < 0)
 	{
-		arr[0] = (-b + sqrt(discriminant)) / (2.0 * a);
-		arr[1] = (-b - sqrt(discriminant)) / (2.0 * a);
+		t = malloc(2 * sizeof(long double));
+		if (!t)
+			return (NULL);
+		t[0] = INF;
+		t[1] = INF;
 	}
-	return (arr);
+	return (t);
 }
 
 t_color	trace_ray(t_data *data, long double *cam_pos, long double *proj_plane,
-		long double min, long double max)
+		const long double *range)
 {
 	t_scn_el	*closest_el;
 	t_scn_el	**spheres;
 	long double	closest_t;
 	long double	*t;
 	int			i;
-	int			bgclr;
 
-	bgclr = 0xFFFFFFFF;
 	closest_t = INF;
 	closest_el = NULL;
 	spheres = get_scn_els(data->scn_el, SPHERE);
@@ -80,13 +79,12 @@ t_color	trace_ray(t_data *data, long double *cam_pos, long double *proj_plane,
 	while (spheres && spheres[i])
 	{
 		t = get_intersection_points(cam_pos, proj_plane, spheres[i]);
-		// printf("t_1: %Lf, t_2: %Lf\n", t[0], t[1]);
-		if (t[0] >= min && t[0] <= max && t[0] < closest_t)
+		if (is_in_range_f(t[0], range[MIN], range[MAX]) && t[0] < closest_t)
 		{
 			closest_t = t[0];
 			closest_el = spheres[i];
 		}
-		if (t[1] >= min && t[1] <= max && t[1] < closest_t)
+		if (is_in_range_f(t[1], range[MIN], range[MAX]) && t[1] < closest_t)
 		{
 			closest_t = t[1];
 			closest_el = spheres[i];
@@ -95,12 +93,8 @@ t_color	trace_ray(t_data *data, long double *cam_pos, long double *proj_plane,
 	}
 	free(spheres);
 	if (closest_el == NULL)
-		return (bgclr);
-	else
-	{
-		// printf("something?\n");
-		return (closest_el->color);
-	}
+		return (data->bg);
+	return (closest_el->color);
 }
 
 void	draw_axes(t_data *data)
@@ -124,29 +118,29 @@ void	draw_axes(t_data *data)
 
 void	render_img(t_data *data)
 {
-	int			x;
-	int			y;
-	t_color		color;
-	long double	*vp_coord;
-	t_scn_el	**cam;
-
+	int					coord[COORD_SIZE];
+	t_color				color;
+	long double			*vp_coord;
+	t_scn_el			**cam;
+	const long double	range[RANGE_SIZE] = {1, INF};
+	
 	cam = get_scn_els(data->scn_el, CAM);
 	if (!cam)
 		error(ft_strdup("No camera found"), EXIT, 1);
-	x = -CANVAS_W / 2;
-	while (x < CANVAS_W / 2)
+	coord[X] = -CANVAS_W / 2;
+	while (coord[X] < CANVAS_W / 2)
 	{
-		y = CANVAS_H / 2;
-		while (y > -CANVAS_H / 2)
+		coord[Y] = CANVAS_H / 2;
+		while (coord[Y] > -CANVAS_H / 2)
 		{
-			vp_coord = canvas_to_viewport(x, y, data->viewport);
-			// printf("x: %Lf, y: %Lf\n",vp_coord[X], vp_coord[Y]);
-			color = trace_ray(data, cam[0]->coord, vp_coord, 1, INF);
-			mlx_put_pixel(data->img, x + CANVAS_W / 2, CANVAS_H / 2 - y, color);
+			vp_coord = canvas_to_viewport(coord[X], coord[Y], data->viewport);
+			color = trace_ray(data, cam[0]->coord, vp_coord, range);
+			mlx_put_pixel(data->img, coord[X] + CANVAS_W / 2, CANVAS_H / 2
+					- coord[Y], color);
 			free(vp_coord);
-			y--;
+			coord[Y]--;
 		}
-		x++;
+		coord[X]++;
 	}
 	draw_axes(data);
 	free(cam);
