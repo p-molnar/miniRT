c2e29049fe72637ec51b621696a9a9c8fb241b34
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-26 11:10:12 +0200

    Update fn prototypes

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:01:05 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/25 16:26:45 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/26 10:49:23 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ void		parse_type_identifier(t_scn_el *el, char *line);
 void		parse_coordinates(long double *coord, char *input);
 void		parse_norm_vec(long double *coord, char *input);
 void		parse_float(long double *f, char *input);
-void		parse_range(long double *f, char *input, long double n_lower, long double n_upper);
+void		parse_range(long double *f, char *input, long double n_lower,
+				long double n_upper);
 void		parse_color(t_color *color, char *input, int n_lower, int n_upper);
 
 //	free
@@ -32,7 +33,8 @@ void		free_arr(void **arr);
 
 //	parser utils
 int			get_arr_size(char **arr);
-int			is_in_range_f(long double n, long double n_lower, long double n_upper);
+int			is_in_range_f(long double n, long double n_lower,
+				long double n_upper);
 int			is_in_range_i(int n, int n_lower, int n_upper);
 int			is_valid_number(char *c);
 
@@ -48,7 +50,9 @@ t_scn_el	**get_scn_els(t_list *list, enum e_scn_el_type type);
 
 //	math
 t_vec		*get_dir_vec(long double *init_point, long double *terminal_point);
-long double		dot(t_vec *vec_1, t_vec *vec_2);
+long double	dot(t_vec *vec_1, t_vec *vec_2);
+long double	*quad_eq_solver(long double a, long double b, long double c,
+				long double *disc);
 
 //	color
 int			get_rgba(int r, int g, int b, int a);
