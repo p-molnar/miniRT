e72d0876ba955866ac7c5deec7bb5e759266a6d0
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-28 14:04:56 +0200

    Add functions, refactor code

--- src/math/matrix_ops.c
+++ src/math/matrix_ops.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/24 15:01:11 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/28 10:29:11 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/28 13:11:07 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,59 +21,23 @@ t_vec	*create_vec(long double *init_point, long double *terminal_point)
 	t_vec				*vec;
 	int					i;
 
+	if (!init_point || !terminal_point)
+		return (NULL);
 	vec = ft_calloc(1, sizeof(t_vec));
 	if (!vec)
 		return (NULL);
-	sum_of_sqrs = 0;
 	i = 0;
 	while (i < COORD_SIZE)
 	{
 		vec->coord[i] = terminal_point[i] - init_point[i];
-		sum_of_sqrs += pow(vec->coord[i], 2);
 		i++;
 	}
-	vec->len = sqrt(sum_of_sqrs);
-	i = 0;
-	while (i < COORD_SIZE)
-	{
-		vec->n_coord[i] = vec->coord[i] / vec->len;
-		i++;
-	}
+	compute_vec_len(vec);
+	compute_normal_vec(vec);
 	return (vec);
 }
 
-long double	*get_normal_vec(t_vec *vec)
-{
-	long double	*coord;
-	long double	vec_len;
-	int			i;
-
-	coord = malloc(1 * sizeof(long double));
-	if (!coord)
-		return (NULL);
-	vec_len = vec->len;
-	i = 0;
-	while (i < COORD_SIZE)
-	{
-		coord[i] = vec->coord[i] / vec_len;
-		i++;
-	}
-	return (coord);
-}
-
-void	scale_vec(long double scaler, t_vec *vec)
-{
-	int	i;
-
-	i = 0;
-	while (i < COORD_SIZE && vec)
-	{
-		vec->coord[i] = scaler * vec->coord[i];
-		i++;
-	}
-}
-
-void	calculate_vec_len(t_vec *vec)
+void	compute_vec_len(t_vec *vec)
 {
 	long double	sum_of_sqrs;
 	int			i;
@@ -83,15 +47,72 @@ void	calculate_vec_len(t_vec *vec)
 	while (i < COORD_SIZE)
 	{
 		sum_of_sqrs += pow(vec->coord[i], 2);
+		i++;
 	}
 	vec->len = sqrt(sum_of_sqrs);
+}
+
+void	compute_normal_vec(t_vec *vec)
+{
+	int	i;
+
+	i = 0;
+	while (i < COORD_SIZE && vec)
+	{
+		vec->n_coord[i] = vec->coord[i] / vec->len;
+		i++;
+	}
+}
+
+t_vec	*scale_vec(long double scaler, t_vec *vec)
+{
+	t_vec	*new_vec;
+	int		i;
+
+	new_vec = ft_calloc(1, sizeof(t_vec));
+	if (!new_vec)
+		return (NULL);
+	i = 0;
+	while (i < COORD_SIZE && vec)
+	{
+		new_vec->coord[i] = scaler * vec->coord[i];
+		i++;
+	}
+	compute_vec_len(new_vec);
+	compute_normal_vec(new_vec);
+	return (new_vec);
 }
 
 long double	dot(t_vec *vec_1, t_vec *vec_2)
 {
 	long double	result;
+	int			i;
 
-	result = vec_1->coord[X] * vec_2->coord[X] + vec_1->coord[Y]
-		* vec_2->coord[Y] + vec_1->coord[Z] * vec_2->coord[Z];
+	result = 0;
+	i = 0;
+	while (i < COORD_SIZE)
+	{
+		result += vec_1->coord[i] * vec_2->coord[i];
+		i++;
+	}
 	return (result);
+}
+
+t_vec	*addition(t_vec *vec_1, t_vec *vec_2)
+{
+	t_vec	*new_vec;
+	int	i;
+
+	new_vec = ft_calloc(1, sizeof(t_vec));
+	if (!new_vec || !vec_1 || !vec_2)
+		return (NULL);
+	i = 0;
+	while (i < COORD_SIZE)
+	{
+		new_vec->coord[i] = vec_1->coord[i] + vec_2->coord[i];
+		i++;
+	}
+	compute_vec_len(new_vec);
+	compute_normal_vec(new_vec);
+	return (new_vec);
 }
