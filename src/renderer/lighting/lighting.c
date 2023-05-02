5adae621446671c6f8500cd65b7e43a79ff58642
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-02 23:33:23 +0200

    Fix code indentation

--- src/renderer/lighting/lighting.c
+++ src/renderer/lighting/lighting.c
@@ -6,14 +6,14 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/28 10:01:12 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/05/02 17:22:35 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/02 23:28:49 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include <minirt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 long double	compute_lighting_intensity(t_data *data, int specular)
 {
@@ -35,20 +35,21 @@ long double	compute_lighting_intensity(t_data *data, int specular)
 			if (lights[i]->type == DIR_LIGHT)
 				data->vec[L] = create_vec(NULL, lights[i]->coord);
 			else if (lights[i]->type == LIGHT)
-				data->vec[L] = create_vec(data->vec[P]->coord, lights[i]->coord);	
+				data->vec[L] = create_vec(data->vec[P]->coord,
+						lights[i]->coord);
 			n_dot_l = dot(data->vec[N], data->vec[L]);
 			if (n_dot_l > 0)
-				intensity += lights[i]->intensity * n_dot_l / (data->vec[L]->len * data->vec[N]->len);
-		}
-		if (specular != -1)
-		{
-			data->vec[Ns] = scale(2 * n_dot_l, data->vec[N]);
-			data->vec[Rv] = subtract(data->vec[Ns], data->vec[L]);
-			data->vec[V] = scale(-1, data->vec[D]);
-			rv_dot_v = dot(data->vec[Rv], data->vec[V]);
-			if (rv_dot_v > 0)
-				intensity += lights[i]->intensity * pow(rv_dot_v / (data->vec[Rv]->len * data->vec[V]->len), specular);
-				
+				intensity += lights[i]->intensity * n_dot_l / (data->vec[L]->len
+						* data->vec[N]->len);
+			if (specular != -1)
+			{
+				data->vec[Ns] = scale(2 * n_dot_l, data->vec[N]);
+				data->vec[Rv] = subtract(data->vec[Ns], data->vec[L]);
+				data->vec[V] = scale(-1, data->vec[D]);
+				rv_dot_v = dot(data->vec[Rv], data->vec[V]);
+				if (rv_dot_v > 0)
+					intensity += lights[i]->intensity * pow(rv_dot_v / (data->vec[Rv]->len * data->vec[V]->len), specular);
+			}
 		}
 		i++;
 	}
