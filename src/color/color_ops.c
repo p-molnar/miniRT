d9dfe56d97b1e893856c49ac433f03db70366d2f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-03 14:37:35 +0200

    Add capping to color updates

--- src/color/color_ops.c
+++ src/color/color_ops.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/24 20:52:46 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/05/01 22:00:08 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/03 14:10:26 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ t_color	update_color_channel(t_color curr_color, t_color new_val, int channel)
 {
 	t_color	col_channel;
 
+	if (new_val < 0)
+		new_val = 0;
+	else if (new_val > 255)
+		new_val = 255;
 	col_channel = get_color(curr_color, channel);
 	curr_color = curr_color & ~(col_channel << (channel * 8));
 	curr_color |= new_val << (channel * 8);
