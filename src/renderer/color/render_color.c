ff47001a21ce0f2ecd4952646492aee4cf477ced
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-04 17:22:50 +0200

    Add fn, update fn name

--- src/renderer/color/render_color.c
+++ src/renderer/color/render_color.c
@@ -6,20 +6,20 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/03 15:57:27 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/05/03 15:57:36 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/04 16:58:21 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-int	compute_color(t_data *data, t_scn_el *closest_el)
+int	get_incident_point_color(t_data *data, t_scn_el *closest_el)
 {
 	t_color		tmp_color;
 	t_color		color;
 	long double	intensity;
 	int			i;
 
-	intensity = compute_lighting_intensity(data, closest_el->specular);
+	intensity = get_lighting_intensity(data, closest_el->specular);
 	color = 0xFFFFFFFF;
 	i = 1;
 	while (i < COLOR_SIZE)
