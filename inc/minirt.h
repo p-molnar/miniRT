77421b6d44d2097235a05e5ca6d14c7750b6df78
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-04 17:21:40 +0200

    Update fn prototypes

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,13 +6,12 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:01:05 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/05/03 16:15:29 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/04 17:14:23 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
-
 # include <mrt_data_struct.h>
 
 //	parser
@@ -42,12 +41,12 @@ void		warning(char *msg);
 
 //	renderer
 void		render_img(t_data *d);
-int	compute_color(t_data *data, t_scn_el *closest_el);
+int	get_incident_point_color(t_data *data, t_scn_el *closest_el);
 
 //	lighting
-long double	compute_lighting_intensity(t_data *data, int specular);
+long double	get_lighting_intensity(t_data *data, int specular);
 long double	get_diffuse_lighting(t_data *data, t_scn_el *light);
-long double	get_specular_lighting(t_data *data, t_scn_el *light, long double specular);
+long double	get_specular_lighting(t_vec **vec, t_scn_el *light, long double specular);
 
 //	util
 t_scn_el	**get_scn_els(t_list *list, int type);
@@ -86,11 +85,11 @@ t_color		get_b(int rgba);
 t_color		get_a(int rgba);
 
 //	render color
-int			compute_color(t_data *data, t_scn_el *closest_el);
+int			get_incident_point_color(t_data *data, t_scn_el *closest_el);
 
 // render util
 void	draw_axes(t_data *data);
-t_closest	*get_closest_el(t_data *data, long double start[3], t_vec *dir, long double range[RANGE_SIZE]);
-long double	*get_intersection_points(t_data *data, long double start[3], t_vec *dir, t_scn_el *obj);
+t_closest	*get_closest_el(t_scn_el **el_arr, long double start_coord[3], t_vec *dir, const long double *range);
+long double	*get_intersection_points(long double start[3], t_vec *dir, t_scn_el *obj);
 long double	*convert_to_viewport(int x, int y, long double *viewport);
 #endif
