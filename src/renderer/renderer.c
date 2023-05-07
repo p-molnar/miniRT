2e70d038c9c854b430ea727bf33492fa2bec23c3
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-08 00:32:52 +0200

    Add rotation matrix formulas

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 11:13:10 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/05/05 09:57:55 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/08 00:24:32 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ t_vec	*get_ray_reflection(t_vec *ray, t_vec *norm)
 
 	if (!ray || !norm)
 		return (NULL);
-	ray_dot_norm = dot(ray, norm);
-	scaled_vec = scale(2 * ray_dot_norm, norm);
+	ray_dot_norm = 2 * dot(ray, norm);
+	scaled_vec = scale(ray_dot_norm, norm);
 	scaled_minus_ray = subtract(scaled_vec, ray);
 	free(scaled_vec);
 	return (scaled_minus_ray);
@@ -43,6 +43,8 @@ t_vec	*get_incident_point(long double *start_coord, t_vec *direction,
 	O = create_vec(start_coord, start_coord);
 	scaled_dir = scale(distance, direction);
 	incident_point = add(O, scaled_dir);
+	free(O);
+	free(scaled_dir);
 	return (incident_point);
 }
 
@@ -52,8 +54,7 @@ t_color	get_reflected_color(t_data *data, t_vec *dir, const long double *range, 
 
 	data->vec[Dneg] = scale(-1, dir);
 	data->vec[Rv] = get_ray_reflection(data->vec[Dneg], data->vec[N]);
-	reflected_color = trace_ray(data, data->vec[P]->coord, data->vec[Rv],
-			range, depth - 1);
+	reflected_color = trace_ray(data, data->vec[P]->coord, data->vec[Rv], range, depth - 1);
 	return (reflected_color);
 }
 
@@ -101,6 +102,29 @@ t_color	trace_ray(t_data *data, long double *start_coord, t_vec *dir,
 	return (mix_colors(color[0], color[1], ref_factor));
 }
 
+long double	*get_rotation_mx(long double theta, t_vec *axis)
+{
+	long double	sin_t;
+	long double	cos_t;
+	long double	*u;
+	long double	*mx;
+
+	sin_t = sin(theta);
+	cos_t = cos(theta);
+	u = axis->n_coord;
+	mx = malloc(9 * sizeof(long double));
+	mx[0] = cos_t + pow(u[0], 2) * (1 - cos_t);
+	mx[1] = u[0] * u[1] * (1 - cos_t) - u[2] * sin_t;
+	mx[2] = u[0] * u[2] * (1 - cos_t) + u[1] * sin_t;
+	mx[3] = u[1] * u[0] * (1 - cos_t) + u[2] * sin_t;
+	mx[4] = cos_t + pow(u[1], 2) * (1 - cos_t);
+	mx[5] = u[1] * u[2] * (1 - cos_t) - u[0] * sin_t;
+	mx[6] = u[2] * u[0] * (1 - cos_t) - u[1] * sin_t;
+	mx[7] = u[2] * u[1] * (1 - cos_t) + u[0] * sin_t;
+	mx[8] = cos_t + pow(u[2], 2) * (1 - cos_t);
+	return (mx);
+}
+
 void	render_img(t_data *data)
 {
 	int					canvas[2];
@@ -109,6 +133,13 @@ void	render_img(t_data *data)
 	long double			*pplane_coord;
 	const long double	range[RANGE_SIZE] = {1, INF};
 
+	long double z_axis[3] = {0, 0, 1};
+
+	t_vec	*z= create_vec(NULL, z_axis);
+	long double angle = rad_to_deg(acos(dot(z, data->cam->n_vec) / (z->len * data->cam->n_vec->len)));
+	t_vec	*rot_axis = cross(z, data->cam->n_vec);
+	long double *mx = get_rotation_mx(angle, rot_axis);
+	printf("ra: %Lf;%Lf;%Lf\n", rot_axis->coord[0], rot_axis->coord[1], rot_axis->coord[2]);
 	canvas[X] = -CANVAS_W / 2;
 	while (canvas[X] < CANVAS_W / 2)
 	{
@@ -119,8 +150,13 @@ void	render_img(t_data *data)
 			screen[Y] = CANVAS_H / 2 - canvas[Y];
 			init_vec(data->vec, VEC_SIZE);
 			pplane_coord = convert_to_viewport(canvas[X], canvas[Y],
-					data->viewport);
+					data->viewport, data->cam->coord);
 			data->vec[D] = create_vec(data->cam->coord, pplane_coord);
+			if (canvas[X] == 0 && canvas[Y] == 0)
+				printf("%Lf; %Lf; %Lf\n", data->vec[D]->coord[0], data->vec[D]->coord[1], data->vec[D]->coord[2]);
+			data->vec[D] = product(mx, data->vec[D]);
+			if (canvas[X] == 0 && canvas[Y] == 0)
+				printf("%Lf; %Lf; %Lf\n", data->vec[D]->coord[0], data->vec[D]->coord[1], data->vec[D]->coord[2]);
 			color = trace_ray(data, data->cam->coord, data->vec[D], range, 2);
 			mlx_put_pixel(data->img, screen[X], screen[Y], color);
 			free_vec(data->vec, VEC_SIZE);
