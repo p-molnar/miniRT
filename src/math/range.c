11365c5c5cd6a20663c2c1f436b49ff751349c82
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-05-08 20:34:50 +0200

    Add function

--- src/math/range.c
+++ src/math/range.c
@@ -6,9 +6,14 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 10:57:31 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/26 10:57:46 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/05/08 20:02:06 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	is_within_range_f(long double n, long double n_lower, long double n_upper)
+{
+	return (n > n_lower && n < n_upper);
+}
 
 int	is_in_range_f(long double n, long double n_lower, long double n_upper)
 {
