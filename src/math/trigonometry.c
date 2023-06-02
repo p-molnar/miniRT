4a81fee10e46574fb4766b368ee18acee3b22e65
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-06-02 13:15:58 +0200

    Add angle operations

--- src/math/trigonometry.c
+++ src/math/trigonometry.c
@@ -6,11 +6,13 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 11:20:13 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/26 11:23:51 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/06/02 11:29:52 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
+#include <minirt.h>
+#include <stdlib.h>
 
 long double	deg_to_rad(long double deg)
 {
@@ -20,4 +22,39 @@ long double	deg_to_rad(long double deg)
 long double	rad_to_deg(long double rad)
 {
 	return (rad * 180 / M_PI);
+}
+
+long double	*get_euler_agls(t_mx *rot_mx)
+{
+	long double	*agls;
+	long double	*m;
+
+	agls = malloc(3 * sizeof(long double));
+	if (!agls)
+		return (NULL);
+	m = rot_mx->m;
+	agls[0] =  atan2(m[9], m[10]);
+	agls[1] =  atan2(-m[8], sqrt(m[9] * m[9] + m[10] * m[10]));
+	agls[2] =  atan2(m[4], m[0]);
+	return (agls);
+}
+
+long double	get_agl_between_r(t_vec3 *vec_1, t_vec3 *vec_2)
+{
+	long double agl;
+
+	if (!vec_1 || !vec_2)
+		return (-1);
+	agl = dot(vec_1, vec_2) / (vec_1->len * vec_2->len);
+	return (acos(agl));
+}
+
+long double	get_agl_between_d(t_vec3 *vec_1, t_vec3 *vec_2)
+{
+	long double	agl;
+
+	agl = get_agl_between_r(vec_1, vec_2);
+	if (agl == -1)
+		return (-1);
+	return (rad_to_deg(agl));
 }
