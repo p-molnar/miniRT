ce99091af9ede0b4e24e21cfcaea1e8eac4aac57
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-07-24 23:45:13 +0200

    Remove redundant fn

--- src/util/util.c
+++ src/util/util.c
@@ -6,25 +6,13 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/24 13:09:33 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/06/10 19:15:17 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/07/24 20:52:06 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 #include <libft.h>
 #include <mrt_data_struct.h>
-
-void	init_vec(t_vec3 **arr, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		arr[i] = NULL;
-		i++;
-	}
-}
 
 t_scn_el	**get_scn_els(t_list *list, enum e_scn_el_type_flags type)
 {
