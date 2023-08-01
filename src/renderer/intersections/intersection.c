0f2d8bb290a0ebc10ac003433574fe2c1ef8f481
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-08-01 12:38:39 +0200

    Fix inc_p tracking for cylinder
    The inc_p was updated by every consequtive cylinder's incident
    points, even if the the particular cylinder is not the closest
    one to the camera. This commit fixes this bug.

--- src/renderer/intersections/intersection.c
+++ src/renderer/intersections/intersection.c
@@ -6,44 +6,45 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/31 12:21:41 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/07/31 12:22:04 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/08/01 12:35:52 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-void	update_hit_obj(t_hit_obj *hit_obj, t_scn_el *el, long double dist,
-		bool is_hit)
+void	update_hit_obj(t_hit_obj *hit_obj, t_hit_obj updated_val)
 {
-	hit_obj->attr = el;
-	hit_obj->dist = dist;
-	hit_obj->is_hit = is_hit;
+	hit_obj->attr = updated_val.attr;
+	hit_obj->dist = updated_val.dist;
+	hit_obj->inc_p_raw = updated_val.inc_p_raw;
+	hit_obj->is_hit = updated_val.is_hit;
 }
 
 t_hit_obj	intersect(t_ray ray, t_scn_el **el, t_range range,
 		enum e_isect mode)
 {
-	t_hit_obj	obj;
+	t_hit_obj	hit_obj;
+	t_coord3	inc_p;
 	long double	t;
 	int			i;
 
-	obj = (t_hit_obj){.is_hit = false, .dist = INF, .attr = NULL};
-	i = 0;
-	while (el && el[i])
+	hit_obj = (t_hit_obj){.is_hit = false, .dist = INF, .attr = NULL};
+	i = -1;
+	while (el && el[++i])
 	{
 		if (el[i]->type == F_SPHERE)
 			t = get_sphere_intersections(ray, el[i]);
 		else if (el[i]->type == F_CYLINDER)
-			t = get_cylinder_intersection(ray, el[i], &obj.inc_p_raw);
+			t = get_cylinder_intersection(ray, el[i], &inc_p);
 		else if (el[i]->type == F_PLANE)
 			t = get_plane_intersection(ray, el[i]);
-		if (is_in_range_f(t, range.min, range.max) && t < obj.dist)
+		if (is_in_range_f(t, range.min, range.max) && t < hit_obj.dist)
 		{
-			update_hit_obj(&obj, el[i], t, true);
+			update_hit_obj(&hit_obj, (t_hit_obj){.attr = el[i], .dist = t,
+				.inc_p_raw = inc_p, .is_hit = true});
 			if (mode == SHADOW)
-				return (obj);
+				return (hit_obj);
 		}
-		i++;
 	}
-	return (obj);
+	return (hit_obj);
 }
