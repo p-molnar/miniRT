f8879409da53a8f27352f86685a4efde321a2448
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-08-09 10:21:36 +0200

    Fix mem leak in titlebar name

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 12:00:14 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/08/07 13:19:33 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/08/09 10:21:23 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@
 int	main(int argc, char *argv[])
 {
 	t_data	data;
+	char	*titlebar;
 
 	ft_memset(&data, 0, sizeof(t_data));
 	parse_input(&data, argc, argv);
 	set_up_scn_el_ptrs(&data);
 	set_up_ctw_mx(&data);
 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
-	data.mlx = mlx_init(CANVAS_W, CANVAS_H, strconcat(3, NAME, " - ", argv[1]),
-			true);
+	titlebar = strconcat(3, NAME, " - ", argv[1]);
+	data.mlx = mlx_init(CANVAS_W, CANVAS_H, titlebar, true);
+	free(titlebar);
 	if (!data.mlx)
 		mlx_err(&data);
 	data.img = mlx_new_image(data.mlx, CANVAS_W, CANVAS_H);
