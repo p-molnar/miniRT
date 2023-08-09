33c82a54a4c402b881270f8109b4a487d902b1ee
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-08-09 10:22:33 +0200

    Update error message

--- inc/mrt_error.h
+++ inc/mrt_error.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/20 21:01:01 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/08/07 22:12:34 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/08/09 10:17:20 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 # define NULL_PTR "NULL Pointer error"
 # define WRONG_EL "Element must be of type: A, L, DL, C, TC, sp, pl, cy"
 # define EMPTY_LINE "Empty line must only consist of a new line character"
-# define INVALID_EXT "Invalid file extension."
+# define INVALID_EXT "Missing or invalid file extension"
 
 # define FMT_E_AMB_LIGHT "Required line format: A <light intensity> <color>"
 # define FMT_E_PNT_LIGHT "Required line format: L <coordinate> \
